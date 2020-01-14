7a4e35f61312fd9015f50aa49b8cb8e2a13a385b
q14781256 <59154265+q14781256@users.noreply.github.com>
2020-01-14 12:05:30 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Admin from '../views/Admin.vue'
+import ClientReprieveLoseList from '../views/cust/ClientReprieveLoseList.vue'
+import ClientServerList from '../views/cust/ClientServerList.vue'
+import SaleChanceList from '../views/sale/SaleChanceList.vue'
+import SalePlanList from '../views/sale/SalePlanList.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: '/admin',
+    name: 'admin',
+    component: Admin,
+    children:[
+      {
+        path: '/admin/clientReprieveLoseList',
+        name: 'clientReprieveLoseList',
+        component: ClientReprieveLoseList
+      },
+      {
+        path: '/admin/clientServerList',
+        name: 'clientServerList',
+        component: ClientServerList
+      },
+      {
+        path: '/admin/saleChanceList',
+        name: 'saleChanceList',
+        component: SaleChanceList
+      },
+      {
+        path: '/admin/salePlanList',
+        name: 'salePlanList',
+        component: SalePlanList
+      }
+    ]
   }
+  
 ]
 
 const router = new VueRouter({
